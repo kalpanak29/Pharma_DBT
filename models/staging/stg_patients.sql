c492dfe8389d87aa671361da8d0497968699c6ce
Kalpana Amrutkar <kamrutkar@nicesoftwaresolutions.com>
2026-04-17 12:21:50 +0530

    Fix is_alive columng

--- models/staging/stg_patients.sql
+++ models/staging/stg_patients.sql
@@ -1,2 +1,12 @@
-SELECT *
+SELECT 
+    patient,
+    birthdate,
+    deathdate,
+    CONCAT(first, ' ', last) AS name,
+    gender,
+    CASE 
+        WHEN deathdate IS NULL THEN 1 
+        ELSE 0 
+    END AS is_alive
 FROM {{ source('bronze', 'patients') }}
+WHERE patient IS NOT NULL
